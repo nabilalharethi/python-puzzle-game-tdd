23986b255288ee1bf88d884fde60cc0329760595
Nabil Abdulbari Abdulqader Al-Harethi <na223bj@student.lnu.se>
2024-12-03 23:46:38 +0100

    GREEN:  test for mocking puzzle type generation

--- tests/puzzle_test.py
+++ tests/puzzle_test.py
@@ -1,5 +1,7 @@
 import pytest
 
+from unittest.mock import patch
+
 from src.puzzle import Puzzle
 
 
@@ -8,3 +10,11 @@ def test_puzzle_type_genration():
     Types = ["Math", "Words", "Logic"]
     generated_types = puzzle.generate_types()
     assert generated_types in Types
+    
+
+
+def test_mock_generate_type():
+    with patch("random.choice", return_value="Math"):
+        puzzle = Puzzle()
+        generated_types = puzzle.generate_types()
+        assert generated_types  == "Math"
