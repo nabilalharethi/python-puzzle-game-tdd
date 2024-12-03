4018cf7f4c36b1618d35936de151c07db8540f40
Nabil Abdulbari Abdulqader Al-Harethi <na223bj@student.lnu.se>
2024-12-04 00:00:41 +0100

    GREEN: Imlimnted seting difficulty

--- src/puzzle.py
+++ src/puzzle.py
@@ -4,6 +4,13 @@ import random
 class Puzzle:
     def __init__(self):
         self.Types = ["Math", "Words", "Logic"]
+        self.difficulty = None
         
     def generate_types(self):
         return random.choice(self.Types)
+    
+    def set_difficulty(self, level):
+        levels = {1: "Easy", 2: "Medium", 3: "Hard"}
+        if level not in levels:
+            raise ValueError("Invalid difficulty level")
+        self.difficulty = levels[level]
