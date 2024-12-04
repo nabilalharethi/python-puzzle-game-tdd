2bd5e2e76a6fba9b82facb29abb2adbb91d4dc78
Nabil Abdulbari Abdulqader Al-Harethi <na223bj@student.lnu.se>
2024-12-04 13:16:07 +0100

    GREEN: Added implimntation to get player name

--- src/ui.py
+++ src/ui.py
@@ -9,3 +9,7 @@ class UI:
     def display_welcome_message(self):
         
         print("Welcome to the Puzzle Game!")
+        
+    def get_player_name(self):
+        
+        return input("Enter your name: ")
