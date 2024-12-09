1c13ea24bf69e1f9396858a35abdb0c02492932f
Andrey Belozerov <ab226ei@student.lnu.se>
2024-12-09 15:01:15 +0100

    GREEN: made my test_game_attributes to pass

--- src/game.py
+++ src/game.py
@@ -4,6 +4,7 @@ import os
 # Add the project root to sys.path
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
 from src.puzzle import Puzzle
+from src.ui import UI
 
 class Game:
     def __init__(self):
@@ -11,6 +12,7 @@ class Game:
         self.game_over = False
         self.score = 0 # would be later changed to instance of score class
         self.lives = 3
+        self.ui = UI()
 
     def start_game(self):
         
