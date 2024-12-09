c79114f73f05ce5f7f524031f7382dc59dcc1de6
Andrey Belozerov <ab226ei@student.lnu.se>
2024-12-09 14:13:13 +0100

    GREEN: made my test_game_attributes pass

--- src/game.py
+++ src/game.py
@@ -1,6 +1,11 @@
+from src.puzzle import Puzzle
+
 class Game:
     def __init__(self):
-        pass
+        self.puzzle = Puzzle()
+        self.game_over = False
+        self.score = 0 # would be later changed to instance of score class
+        self.lives = 3
 
     def start_game():
         pass
