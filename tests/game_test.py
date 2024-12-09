95474f42e47bc3d55e4c8906bde2ac829939ca33
Andrey Belozerov <ab226ei@student.lnu.se>
2024-12-09 14:07:45 +0100

    RED: wrote test_game_attributes to test that GAme class constructor has specific attributes

--- tests/game_test.py
+++ tests/game_test.py
@@ -16,4 +16,12 @@ class TestGame:
         return Game  # Provide the Game class as a fixture
     
     def test_startgame_exists(self):
+        #create sut variable 
         assert hasattr(Game, 'start_game')
+
+    def test_game_attributes(self):
+        sut = Game()
+        assert hasattr(sut, 'puzzle') # Checks if constructor has puzzle instance
+        assert hasattr(sut, 'game_over') # Checks if constructor has game_over instance
+        assert hasattr(sut, 'score') # Checks if constructor has score instance
+        assert hasattr(sut, 'lives') # Checks if constructor has lives instance
