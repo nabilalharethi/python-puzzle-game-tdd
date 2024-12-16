b6db81d4889e4c47b9cedec4a61fbcad2bcb063f
Nabil Abdulbari Abdulqader Al-Harethi <na223bj@student.lnu.se>
2024-12-16 02:19:35 +0100

    Green: Aded method to add multiple points

--- src/score.py
+++ src/score.py
@@ -3,6 +3,7 @@ class Score:
     def __init__(self):
         self._current_score = 0
         self._high_score = 0
+        self._score_multiplier = 1
         
     def get_current_score(self):
 
@@ -21,7 +22,7 @@ class Score:
         if points < 0:
             raise ValueError("Points cannot be negative")
         
-        self._current_score += points
+        self._current_score += points * self._score_multiplier
         
     def reset_score(self):
 
@@ -32,3 +33,10 @@ class Score:
      
         if self._current_score > self._high_score:
             self._high_score = self._current_score
+            
+    def set_score_multiplier(self, multiplier):
+
+        if not isinstance(multiplier, int) or multiplier < 1:
+            raise ValueError("Multiplier must be a positive integer")
+        
+        self._score_multiplier = multiplier
