b10e3573e116bc3594b996b6623ae0aa159347ae
Nabil Abdulbari Abdulqader Al-Harethi <na223bj@student.lnu.se>
2024-12-16 01:06:11 +0100

    RED: Added initial score test

---
+++ tests/score_test.py
@@ -0,0 +1,17 @@
+
+import pytest
+
+from unittest.mock import patch
+
+from src.score import Score
+class TestScore:
+    def setup_method(self):
+
+        self.score = Score()
+    
+    def test_initial_score(self):
+
+        assert self.score.get_current_score() == 0
+        assert self.score.get_high_score() == 0
+        
+        
